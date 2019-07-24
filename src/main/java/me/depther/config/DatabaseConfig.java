e75acc7aa57a13f7285d5c2a361e2203f4fca483
Depther <victory2373@gmail.com>
2019-07-24 18:51:42 +0900

    commit

--- src/main/java/me/depther/config/DatabaseConfig.java
+++ src/main/java/me/depther/config/DatabaseConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
