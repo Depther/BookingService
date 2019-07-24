666f132553305d766beb876b1c57c8cd38bf8ecb
Depther <victory2373@gmail.com>
2019-07-24 15:55:32 +0900

    프로파일 기능 추가

--- src/main/java/me/depther/config/DatabaseConfig.java
+++ src/main/java/me/depther/config/DatabaseConfig.java
@@ -5,6 +5,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,25 +18,54 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource("classpath:/application.properties")
 public class DatabaseConfig implements TransactionManagementConfigurer {
 
-	@Autowired
-	private Environment environment;
+	@Configuration
+	@Profile("dev")
+	@PropertySource("classpath:/application.properties")
+	public static class DevConfig {
 
-	@Bean
-	public HikariConfig hikariConfig() {
-		HikariConfig hikariConfig = new HikariConfig();
-		hikariConfig.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
-		hikariConfig.setJdbcUrl(environment.getProperty("spring.datasource.url"));
-		hikariConfig.setUsername(environment.getProperty("spring.datasource.username"));
-		hikariConfig.setPassword(environment.getProperty("spring.datasource.password"));
-		return hikariConfig;
+		@Autowired
+		private Environment environment;
+
+		@Bean
+		public HikariConfig hikariConfig() {
+			HikariConfig hikariConfig = new HikariConfig();
+			hikariConfig.setDriverClassName(environment.getProperty("dev.spring.datasource.driver-class-name"));
+			hikariConfig.setJdbcUrl(environment.getProperty("dev.spring.datasource.url"));
+			hikariConfig.setUsername(environment.getProperty("dev.spring.datasource.username"));
+			hikariConfig.setPassword(environment.getProperty("dev.spring.datasource.password"));
+			return hikariConfig;
+		}
+
 	}
+
+	@Configuration
+	@Profile("real")
+	@PropertySource("classpath:/application.properties")
+	public static class RealConfig {
+
+		@Autowired
+		private Environment environment;
+
+		@Bean
+		public HikariConfig hikariConfig() {
+			HikariConfig hikariConfig = new HikariConfig();
+			hikariConfig.setDriverClassName(environment.getProperty("real.spring.datasource.driver-class-name"));
+			hikariConfig.setJdbcUrl(environment.getProperty("real.spring.datasource.url"));
+			hikariConfig.setUsername(environment.getProperty("real.spring.datasource.username"));
+			hikariConfig.setPassword(environment.getProperty("real.spring.datasource.password"));
+			return hikariConfig;
+		}
+
+	}
+
+	@Autowired
+	private HikariConfig hikariConfig;
 
 	@Bean
 	public DataSource dataSource() {
-		return new HikariDataSource(hikariConfig());
+		return new HikariDataSource(hikariConfig);
 	}
 
 	@Override
